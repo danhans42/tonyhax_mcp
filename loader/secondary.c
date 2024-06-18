9273e6dca342aa862a169f8295076b58a24c71c2
Dan H <dan.hansbro@gmail.com>
2024-06-18 17:34:09 +0100

    Add Licence info for MemCard Pro Library

--- loader/secondary.c
+++ loader/secondary.c
@@ -1,3 +1,30 @@
+/*  
+Tonyhax with MCP/SD2PSX etc support added
+
+https://orca.pet/tonyhax/index.html
+https://github.com/socram8888/tonyhax
+
+Implements the protocol which can be found here https://gitlab.com/chriz2600/ps1-game-id-transmission 
+utilising the library/code found here https://github.com/Cybdyn-Systems/MemCardPro-ASM
+
+--Memcard Pro Libary Licence Notice--
+
+MemCard Pro Library
+Copyright (C) 2021-2024 Cybdyn Systems. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
 
 #include <stdbool.h>
 #include <stdint.h>
